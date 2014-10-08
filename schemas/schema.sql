53314aee9dab086315ab0f84d056c105f2f9f385
Khanh Tran <khanh@glandoresystems.com>
2014-10-08 17:58:55 +0700

    Add schema

---
+++ schemas/schema.sql
@@ -0,0 +1,47 @@
+/*
+SQLyog Community v11.5 (64 bit)
+MySQL - 5.5.39-MariaDB-0ubuntu0.14.04.1 : Database - tsuru-services
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tsuru-services` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `tsuru-services`;
+
+/*Table structure for table `env` */
+
+DROP TABLE IF EXISTS `env`;
+
+CREATE TABLE `env` (
+  `name` varchar(64) NOT NULL,
+  `code` varchar(64) DEFAULT NULL,
+  `value` text NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `instance` */
+
+DROP TABLE IF EXISTS `instance`;
+
+CREATE TABLE `instance` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `team` varchar(64) DEFAULT NULL,
+  `plan` varchar(64) DEFAULT NULL,
+  `params` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
