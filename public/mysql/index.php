86a7706ad21d874f98c8eaa4b9f62fc9decd6ae4
Khanh Tran <khanh@glandoresystems.com>
2014-10-08 15:59:28 +0700

    done for bind and unbind (still some errors).

--- public/mysql/index.php
+++ public/mysql/index.php
@@ -1,5 +1,5 @@
 <?php
-$config = new \Phalcon\Config\Adapter\Ini(__DIR__ . '/config/config.ini');
+$config = new \Phalcon\Config\Adapter\Ini(__DIR__ . '/config.ini');
 
 // Use Loader() to autoload our model
 $loader = new \Phalcon\Loader();
@@ -82,18 +82,37 @@ $app->post('/resources', function () use ($app) {
 
 /**
  * Bind
- * http://docs.tsuru.io/en/latest/services/build.html#creating-new-instances
+ * http://docs.tsuru.io/en/latest/services/build.html#bind
  */
 $app->post('/resources/{name:[a-zA-Z0-9\-]+}', function ($name) use ($app) {
 
-    $appHost = $app->request->getQuery('app-host');
-    $unitHost = $app->request->getQuery('unit-host');
+    $appHost = $app->request->getPost('app-host');
+    $unitHost = $app->request->getPost('unit-host');
 
     /** @var Phalcon\Db\Adapter\Pdo $db */
     $db = $app->getDI()->getShared('db');
+
+    $test = $db->query("SELECT * FROM env WHERE name = '$appHost' OR code='$unitHost'")->fetch();
+    if ($test) {
+        $params = json_decode($test['value']);
+        $params = [
+            'MYSQL_DATABASE_NAME' => $params->MYSQL_DATABASE_NAME,
+            'MYSQL_HOST' => $params->MYSQL_HOST,
+            'MYSQL_PORT' => $params->MYSQL_PORT,
+            'MYSQL_USER' => $params->MYSQL_USER,
+            'MYSQL_PASSWORD' => $params->MYSQL_PASSWORD
+        ];
+        $response = new \Phalcon\Http\Response();
+        $response->setStatusCode(201, "");
+        $content = json_encode($params);
+        $response->setContent($content);
+        return $response;
+    }
+
+    $userName = 'u_' . rand(10, 99) . '_' .time();
     $password = md5($name . time() . rand(100, 999));
-    $db->execute("CREATE USER '$appHost'@'%' IDENTIFIED BY '$password'; ");
-    $db->execute("GRANT ALL PRIVILEGES ON $name TO '$appHost'@'%';");
+    $db->execute("CREATE USER '$userName'@'%' IDENTIFIED BY '$password'; ");
+    $db->execute("GRANT ALL PRIVILEGES ON `$name`.* TO '$userName'@'%';");
     $db->execute("FLUSH PRIVILEGES;");
 
     $row = $db->query("SELECT * FROM instance WHERE name = '$name'")->fetch();
@@ -101,9 +120,34 @@ $app->post('/resources/{name:[a-zA-Z0-9\-]+}', function ($name) use ($app) {
     $response = new \Phalcon\Http\Response();
     $response->setStatusCode(201, "");
     $params = json_decode($row['params']);
-    $params['MYSQL_USER'] = $appHost;
-    $params['MYSQL_PASSWORD'] = $password;
-    $response->setContent(json_encode($params));
+    $params = [
+        'MYSQL_DATABASE_NAME' => $params->MYSQL_DATABASE_NAME,
+        'MYSQL_HOST' => $params->MYSQL_HOST,
+        'MYSQL_PORT' => $params->MYSQL_PORT,
+        'MYSQL_USER' => $userName,
+        'MYSQL_PASSWORD' => $password
+    ];
+    $content = json_encode($params);$response->setContent($content);
+    $db->insert('env', [$appHost, $unitHost, $content], ['name', 'code', 'value']);
+    return $response;
+});
+
+/**
+ * Unbinding instances from apps
+ * http://docs.tsuru.io/en/latest/services/build.html#removing-instances
+ */
+$app->delete('/resources/{name:[a-zA-Z0-9\-]+}/hostname/{host:[a-zA-Z0-9\-\.]+}', function ($name, $host) use ($app) {
+    /** @var Phalcon\Db\Adapter\Pdo $db */
+    $db = $app->getDI()->getShared('db');
+
+    $row = $db->query("SELECT * FROM env WHERE name = '$name' OR code='$host'")->fetch();
+    $params = json_decode($row['value']);
+
+    $db->delete('env', "id={$row['id']}");
+    $db->execute("DROP USER '{$params->MYSQL_USER}'@'%';");
+
+    $response = new \Phalcon\Http\Response();
+    $response->setStatusCode(401, "");
     return $response;
 });
 
